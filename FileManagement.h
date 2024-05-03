0944f7d24589efe76be2aea509a67af4ba9ca469
ayesha ashfaq <158320332+AyeshaAshfaq22@users.noreply.github.com>
2024-05-03 23:00:58 +0500

    Create FileManagement.h

---
+++ FileManagement.h
@@ -0,0 +1,9 @@
+#ifndef FILEMANAGEMENT_H
+#define FILEMANAGEMENT_H
+
+#include "RepositoryManagement.h"
+
+void AddFile(Repository* repo, const string& fileName);
+void DeleteFile(Repository* repo, const string& fileName);
+
+#endif
